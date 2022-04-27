96044820941c299cac46c0ec01b1568cbbcc6e53
Joshua McGehee <mcgehee.joshua@gmail.com>
2022-04-27 17:38:11 -0600

    experimented with event listeners

--- dom.js
+++ dom.js
@@ -123,3 +123,81 @@ const itemList = document.querySelector('#items')
 // newDiv.style.fontSize = '30px'
 
 // console.log(newDiv);
+
+// function buttonClick() {
+//   console.log('button clicked');
+// } //Old way of doing thins. 
+
+// ADDING FUNCTIONALITY WITH EVENT LISTENERS
+
+// CLICK EVENTS
+
+// const button = document.getElementById('button').addEventListener('click', buttonClick) //Event listener
+
+// function buttonClick(event) {
+//   // console.log('Button clicked');
+//   // document.getElementById('header-title').textContent = 'Changed'
+//   // document.querySelector('#main').style.backgroundColor = '#f4f4f4'
+//   // console.log(event);
+//   // console.log(event.target);
+//   // console.log(event.target.id, event.target.className);
+let output = document.getElementById('output')
+//   // output.innerHTML = '<h3>'+event.type+'</h3>'
+
+//   // console.log(event.clientX, event.clientY);
+//   // console.log(event.offsetX, event.offsetY);
+
+//   console.log(event.altKey);
+//   console.log(event.shiftKey);
+//   console.log(event.ctrlKey);
+// } // oeprative function
+
+// MOUSE EVENTS
+
+const button = document.getElementById('button')
+const box = document.getElementById('box')
+
+
+// button.addEventListener('click', runEvent) //Event listener
+// button.addEventListener('dblclick', runEvent)
+// button.addEventListener('mousedown', runEvent)
+// button.addEventListener('mouseup', runEvent)
+
+// box.addEventListener('mouseenter', runEvent)
+// box.addEventListener('mouseleave', runEvent)
+// box.addEventListener('mouseover', runEvent)
+// box.addEventListener('mouseout', runEvent)
+// box.addEventListener('mousemove', runEvent)
+
+let itemInput = document.querySelector('input[type="text"]')
+// let submitButton = document.querySelector('input[type="submit"]')
+let form = document.querySelector('form')
+let select = document.querySelector('select')
+
+// itemInput.addEventListener('keydown', runEvent)
+// itemInput.addEventListener('keypress', runEvent)
+// submitButton.addEventListener('click', runEvent)
+// itemInput.addEventListener('focus', runEvent) //when you click into an element
+// itemInput.addEventListener('blur', runEvent) //when you click out of an event
+
+// itemInput.addEventListener('input', runEvent) //when you do anything with an input
+// select.addEventListener('change', runEvent)
+// select.addEventListener('input', runEvent)
+
+form.addEventListener('submit', runEvent)
+
+function runEvent(e) {
+  e.preventDefault()
+  console.log('EVENT TYPE: '+e.type);
+  // console.log(e.target.value);
+  // output.innerHTML = `<h3>${ e.target.value }</h3>`
+  // e.preventDefault() // Stopped the page from being reloaded. 
+  // output.innerHTML = `<h3>${ itemInput.value }</h3>`
+
+  // output.innerHTML = '<h3>MouseX: ' +e.offsetX+ '</h3><h3>MouseY: ' +e.offsetY+ '</h3>'
+
+  // box.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},${e.offsetX - e.offsetY})`
+  //   document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},${e.offsetX - e.offsetY})`
+  // 
+  console.log(e.target.value);
+}
